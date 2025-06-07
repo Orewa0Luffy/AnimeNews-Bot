0f93708d54a25b6c86d42efd417a97dbeb35a7ef
Orewa0Luffy <ichigobotam1@gmail.com>
2025-06-07 12:49:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ BOT_TOKEN = "" #Bot Token get it from @BotFather
 URL_A = "https://www.animenewsnetwork.com/all/rss.xml" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+MONGO_URI = "mongodb+srv://f00il:animeotaku109@cluster0.qhc7amc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
 ADMINS = "" #Admin ID
