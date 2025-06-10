d624fdcc93bdbddce334eb19ccfa77975aa71085
Orewa0Luffy <ichigobotam1@gmail.com>
2025-06-10 11:28:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 
 API_ID = "28713982" #API ID get it from telegram.org
 API_HASH = "237e15f7c006b10b4fa7c46fee7a5377" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+BOT_TOKEN = "7811648626:AAFhQ7XcV7jGMMUSJisUKc01f0X9eIi0Ajw" #Bot Token get it from @BotFather
 URL_A = "https://www.animenewsnetwork.com/all/rss.xml" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
+START_PIC = "https://envs.sh/aWQ.jpg" #Startup Pic
 MONGO_URI = "mongodb+srv://f00il:animeotaku109@cluster0.qhc7amc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+ADMINS = "7868834444" #Admin ID
